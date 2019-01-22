85d0aa846de2c4ab2847ba058f2e1e6ae4cee756
etudiant <etudiant@imac-de-informatique-2.local>
2019-01-22 16:21:47 +0100

    ajout détails, des ViewController, tableViewCell

---
+++ Cocktail/DetailsCocktailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailsCocktailViewController.swift
+//  Cocktail
+//
+//  Created by etudiant on 22/01/2019.
+//  Copyright © 2019 ViNiKi. All rights reserved.
+//
+
+import UIKit
+
+class DetailsCocktailViewController: UIViewController {
+
+    @IBOutlet weak var boutonAjoutFavoris: UIButton!
+    @IBOutlet weak var imageDuCocktails: UIImageView!
+    @IBOutlet weak var nomCocktail: UILabel!
+    @IBOutlet weak var descriptionCocktail: UILabel!
+    @IBOutlet weak var tableIngredient: UITableView!
+    @IBOutlet weak var instructionCocktail: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func ajouterAuxFavoris(_ sender: Any) {
+    }
+}
---
+++ Cocktail/IngredientTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  IngredientTableViewCell.swift
+//  Cocktail
+//
+//  Created by etudiant on 22/01/2019.
+//  Copyright © 2019 ViNiKi. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+    
+    // Ingredient et la quantité
+    @IBOutlet weak var ingredientMesure: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    /*
+	"idDrink":"15997"
+    "strDrink":"GG"
+    "strCategory":"Ordinary Drink"
+    "strIBA":null
+    "strAlcoholic":"Optional alcohol",
+    "strGlass":"Collins Glass",
+    "strInstructions":"Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.",
+    "strDrinkThumb":"https:\/\/www.thecocktaildb.com\/images\/media\/drink\/vyxwut1468875960.jpg",
+    "strIngredient1":"Galliano",
+    "strIngredient2":"Ginger ale",
+    "strIngredient3":"Ice",
+    "strIngredient4":"",
+    "strIngredient5":"",
+    "strIngredient6":"",
+    "strIngredient7":"",
+    "strIngredient8":"",
+    "strIngredient9":"",
+    "strIngredient10":"",
+    "strIngredient11":"",
+    "strIngredient12":"",
+    "strIngredient13":"",
+    "strIngredient14":"",
+    "strIngredient15":"",
+    "strMeasure1":"2 1\/2 shots ",
+    "strMeasure2":"\n",
+    "strMeasure3":" ",
+    "strMeasure4":" ",
+    "strMeasure5":" ",
+    "strMeasure6":" ",
+    "strMeasure7":" ",
+    "strMeasure8":"",
+    "strMeasure9":"",
+    "strMeasure10":"",
+    "strMeasure11":"",
+    "strMeasure12":"",
+    "strMeasure13":"",
+    "strMeasure14":"",
+    "strMeasure15":"",
+ */
+}
